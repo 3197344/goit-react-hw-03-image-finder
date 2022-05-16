25e70999e987ca248ab9bd6fe56d441b25bb82cd
Nadia Lut <3197344@gmail.com>
2022-05-16 10:20:28 +0300

    Button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template. Hello
-    </div>
-  );
-};
+import { Component } from "react";
+// import Searchbar from "./Searchbar/Searchbar";
+// import ImageGallery from './ImageGallery/ImageGallery'
+// import Loader from './Loader/Loader'
+import Button from "./Button/Button";
+// import Modal from "./Modal/Modal";
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <Searchbar />
+        <ImageGallery />
+        <Loader /> */}
+        <Button />
+        {/* <Modal /> */}
+      </div>
+    )
+  }
+}
+
+
+// import Button from "./Button/Button";
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template. Hello
+//       <Button/>
+//     </div>
+//   );
+// };
